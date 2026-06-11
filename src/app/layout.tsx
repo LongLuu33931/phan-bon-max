fccf7bbe07241846faed0e88525ed63f3a22abdc
Long.Luu <luuthanhlong24@gmail.com>
2026-06-11 18:51:18 +0700

    Add Facebook Pixel tracking script to RootLayout for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Be_Vietnam_Pro } from "next/font/google";
+import Script from "next/script";
 import { ToastProvider } from "@/components/toast-provider";
 import "./globals.css";
 
@@ -54,6 +55,31 @@ export default function RootLayout({
             <body className="flex min-h-full flex-col">
                 {children}
                 <ToastProvider />
+                <Script
+                    id="meta-pixel"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '975422638556284');
+fbq('track', 'PageView');`,
+                    }}
+                />
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: "none" }}
+                        src="https://www.facebook.com/tr?id=975422638556284&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
             </body>
         </html>
     );
